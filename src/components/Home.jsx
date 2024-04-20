f55f3f17914a26e95845e989429c84922c61876c
Code-with-Nani <usmanhanan02@gmail.com>
2024-04-20 09:58:41 +0500

    initialized

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-screen min-h-screen bg-bg">
+      <h1 className="heading text-[16vw] leading-[14vw] tracking-tighter font-[600]">
+        CHANGE <br /> THE COURSE
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
