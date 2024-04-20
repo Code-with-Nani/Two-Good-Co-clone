8327f211164c12df8b689dd0cd5b6f778531a9eb
Code-with-Nani <usmanhanan02@gmail.com>
2024-04-20 16:30:38 +0500

    cards finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
 import Cards from "./components/Cards";
 import Home from "./components/Home";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  const container = useRef();
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll({
+      el: container.current,
+      smooth: true,
+      multiplier: 1,
+    });
+  }, []);
+
   return (
-    <div className="main overflow-hidden selection:text-bg selection:bg-zinc-950 bg-bg ">
+    <div
+      ref={container}
+      className="main overflow-hidden selection:text-bg selection:bg-zinc-950 bg-bg "
+    >
       <Home />
       <Cards />
     </div>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,22 +5,41 @@ import img3 from "../assets/images/Two Good Co - Premium Range.jpg";
 
 const Cards = () => {
   return (
-    <div className="w-screen min-h-screen bg-red-600 lg:px-7 lg:pr-12 px-3 py-4">
+    <div className="w-screen min-h-screen lg:px-7 lg:pr-12 px-3 py-4">
       <div className="text flex justify-between items-center border-b-[1px] border-b-black border-opacity-70 text-sm">
         {["BUY GOOD", "DO GOOD"].map((item) => (
           <h2>{item}</h2>
         ))}
       </div>
-      <div className="cards h-[100vh] w-full relative flex items-center justify-between mt-3">
-        <div className="card h-full w-[32.4%] overflow-hidden">
-          <img className="w-full h-full object-cover object-bottom" src={img1} alt="" />
-        </div>
-        <div className="card h-full w-[32.4%] overflow-hidden">
-          <img className="w-full h-full object-cover object-bottom" src={img2} alt="" />
-        </div>
-        <div className="card h-full w-[32.4%] overflow-hidden">
-          <img className="w-full h-full object-cover object-bottom" src={img3} alt="" />
-        </div>
+      <div className="cards lg:h-[100vh] w-full relative flex flex-col lg:flex-row items-center justify-between mt-3 gap-5 lg:gap-0">
+        {[img1, img2, img3].map((item, index) => (
+          <div
+            key={index}
+            className="card w-full h-[50vh] lg:h-full lg:w-[32.4%] overflow-hidden relative flex justify-center items-center"
+          >
+            <img
+              data-scroll
+              data-scroll-section
+              data-scroll-speed="-0.1"
+              className="w-full h-full object-cover object-center overflow-hidden"
+              src={item}
+              alt=""
+            />
+            <div
+              data-scroll
+              data-scroll-section
+              data-scroll-speed="-0.2"
+              className={`btn absolute h-12  w-[75%] flex justify-between items-center px-10 rounded-full ${
+                index === 0 && "bg-[#f5dfcf]"
+              } ${index === 1 && "bg-[#edf3ff]"} ${index === 2 && "bg-[#fff]"}`}
+            >
+              <div className="w-2 h-2 bg-black rounded-full"></div>
+              <h2 className="">SHOP</h2>
+              <h2 className="text-[800]">HOME</h2>
+              <div className="">›</div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
